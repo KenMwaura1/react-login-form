50050746e89bb65d2342ea99dbe69d3567c14a24
KenMwaura1 <kemwaura@gmail.com>
2020-08-03 22:04:29 +0300

    Updated main app with Login form

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import {
   ColorModeProvider,
   CSSReset
 } from '@chakra-ui/core';
-import ThemeToggler from './components/ThemeToggler'
+import ThemeToggler from './components/ThemeToggler';
+import LoginForm from './pages/LoginForm';
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <ColorModeProvider>
         <CSSReset />
         <ThemeToggler />
+        <LoginForm />
       </ColorModeProvider>
     </ThemeProvider>
   );
--- src/pages/LoginForm.js
+++ src/pages/LoginForm.js
@@ -12,7 +12,22 @@ export default function LoginForm() {
         <Flex width="Full" align="center" justifyContent="center">
             <Box p={2}>
                 <Box textAlign="center">
-                    <Heading> Login</Heading>
+                    <Heading> Login </Heading>
+                </Box>
+                <Box my={4} textAlign="left">
+                    <form>
+                        <FormControl>
+                            <FormLabel> Email </FormLabel>
+                            <Input type="email" placeholder="test@test.com" />
+                        </FormControl>
+                        <FormControl mt={6}>
+                            <FormLabel> Password </FormLabel>
+                            <Input type="password" placeholder="********" />
+                        </FormControl>
+                        <Button width="full" mt={4} type="submit">
+                            Sign In 
+                        </Button>
+                    </form>
                 </Box>
             </Box>
         </Flex>
